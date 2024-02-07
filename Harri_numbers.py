1a64a5978f0744a75e0f69a70c9fd585eff5389d
StenoHarri <159228685+StenoHarri@users.noreply.github.com>
2024-02-07 08:40:53 +0000

    Implimented @scarletcanine ideas
    There are now starter chords, suffixes and prefixes

--- Harri_numbers.py
+++ Harri_numbers.py
@@ -26,6 +26,31 @@ import re
 
 LONGEST_KEY = 4
 
+
+starter_chord = {
+    #In plover, {&} in the outline, means treat it like fingerspelling
+    "#*"   : {"prefix"       : "{&",
+              "suffix(one)"  : "}",
+              "suffix(many)" : "}"},
+
+    "#"    : {"prefix"       : " ",
+              "suffix(one)"  : " ",
+              "suffix(many)" : " "},
+
+    "#KWR" : {"prefix"       : " ",
+              "suffix(one)"  : " year ",
+              "suffix(many)" : " years "},
+
+    "#HRAO"  : {"prefix"     : " oh look, ",
+              "suffix(one)"  : " mouse ",
+              "suffix(many)" : " mice "}
+}
+
+linker_chord = { #by linker, I mean, what gets combined into the zeroes
+    "#"   : True
+}
+
+
 primary_number = {
     ""    : "",
     "R"   : "1",
@@ -76,6 +101,9 @@ def end_zeros(string):
     count = len(string) - len(string.rstrip("0"))
     return count
 
+
+
+
 def lookup(strokes):
     
     assert len(strokes) <= LONGEST_KEY, '%d/%d' % (len(strokes), LONGEST_KEY)
@@ -87,19 +115,20 @@ def lookup(strokes):
         if any(single_digit_number in "0123456789" for single_digit_number in strokes[stroke_number]):
             strokes=list(strokes)
             strokes[stroke_number]= "#" + strokes[stroke_number].replace(
-                "3","P").replace(
-                    "6", "F").replace(
-                        "7", "P").replace(
-                            "8", "L").replace(
-                                "9", "T")
+                "0", "O").replace(
+                    "1", "S").replace(
+                        "2", "T").replace(
+                            "3", "P").replace(
+                                "4", "H").replace(
+                                    "5", "A").replace(
+                                        "6", "F").replace(
+                                            "7", "P").replace(
+                                                "8", "L").replace(
+                                                    "9", "T")
 
 
-        #For the first stroke, P is allowed
-        if stroke_number==0:
-            match = re.fullmatch(r'#(\*?)-?([EU]*)([FRPB]*)([LGTS]*)', strokes[stroke_number])
-        else:
-            match =    re.fullmatch(r'#()-?([EU]*)([FRPB]*)([LGTS]*)', strokes[stroke_number])
-
+        match = re.fullmatch(r'(#?\^?S?T?K?P?W?H?R?A?O?\*?)-?([EU]*)([FRPB]*)([LGTS]*)', strokes[stroke_number])
+ 
         #if it's not valid
         if not match:
             raise KeyError
@@ -109,10 +138,18 @@ def lookup(strokes):
 
 
         if stroke_number==0:
+            if not (starter_chord[match[1]]):
+                raise KeyError
+            prefix = starter_chord[match[1]]['prefix']
+            suffix_one = starter_chord[match[1]]['suffix(one)']
+            suffix_many = starter_chord[match[1]]['suffix(many)']
+
             output_number+=(primary_number[match[3]]+
                             secondary_number[match[4]]+
                             trailing_zeroes[match[2]])
         else:
+            if not (linker_chord[match[1]]):
+                raise KeyError
             successive_numbers=(primary_number[match[3]]+
                                 secondary_number[match[4]]+
                                 trailing_zeroes[match[2]])
@@ -120,9 +157,12 @@ def lookup(strokes):
                 raise KeyError
             output_number = (output_number[0:len(output_number)-len(successive_numbers)]+
                              successive_numbers)
-
-
-    if '*' in strokes[0] or output_number[0]==',':
-        return '{^'+output_number+'}'
+    
+    if output_number=='1':
+        suffix=suffix_one
     else:
-        return ' '+output_number
+        suffix=suffix_many
+    
+    return (prefix+
+            output_number +
+            suffix)
